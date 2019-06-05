cf0d2d6ef390d2477cc0028e69184dd36e61f24b
Toni March <amarch@brujula.es>
2019-06-05 09:10:55 +0200

    Created a method in data and import data in home component

--- TutorialAngular/tutorialAngular/src/app/home/home.component.ts
+++ TutorialAngular/tutorialAngular/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+
 
 @Component({
   selector: 'app-home',
@@ -9,13 +11,13 @@ export class HomeComponent implements OnInit {
 
   h1Style: boolean = false;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
   }
 
   firstClick(){
-    this.h1Style = true;
+    this.data.firstClick();
   }
 
 }
